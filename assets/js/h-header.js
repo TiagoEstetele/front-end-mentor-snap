4e2dd9ac026ba8331fb56f2c75191bca28cd26d4
Tiago Estetele <tiago.estetele@okn.com.br>
2023-08-20 16:02:18 -0300

    building project

--- assets/js/h-header.js
+++ assets/js/h-header.js
@@ -1,38 +1,89 @@
-const headerButtons = $$j('.h-header__links > ul > li');
+if(window.innerWidth > 900) {
+    desktopHeader();
+} else {
+    mobileHeader();
+}
 
-headerButtons.forEach(button => {
-    button.addEventListener('mouseenter', function() {
-        const isDropdown = this.querySelector('.h-header__dropdown') !== null;
-        const dropdown =  this.querySelector('.h-header__dropdown');
-        const svgElement = button.querySelector('a svg');
-        
 
-        if(isDropdown) {
-           dropdown.toggleAttribute('show');
-           svgElement.toggleAttribute('animate');
+function desktopHeader() {
 
-        }
+    const headerButtons = $$j('.h-header__links > ul > li');
 
+    headerButtons.forEach(button => {
+        button.addEventListener('mouseenter', function() {
+            const isDropdown = this.querySelector('.h-header__dropdown') !== null;
+            const dropdown =  this.querySelector('.h-header__dropdown');
+            const svgElement = button.querySelector('a svg');
+            
+
+            if(isDropdown) {
+            dropdown.toggleAttribute('show');
+            svgElement.toggleAttribute('animate');
+
+            }
+
+        })
+
+        button.addEventListener('mouseleave', function() {
+            const dropdown =  this.querySelector('.h-header__dropdown');
+            const svgElement = button.querySelector('a svg');
+
+            if(dropdown !== null && dropdown.hasAttribute('show')) {
+                dropdown.removeAttribute('show');
+                svgElement.removeAttribute('animate');
+            }
+        })
+    });
+
+
+    window.addEventListener('DOMContentLoaded', function() {
+        headerButtons.forEach(( button ) => {
+            const isDropdown = button.querySelector('.h-header__dropdown') !== null;
+            const svgElement = button.querySelector('a svg');
+
+            if (isDropdown) {
+                svgElement.toggleAttribute('icon'); 
+            }
+        })
+    });
+}
+
+function mobileHeader() {
+      
+    const headerButtons = $$j('.h-header__links > ul > li');
+
+    headerButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const isDropdown = this.querySelector('.h-header__dropdown') !== null;
+            const dropdown =  this.querySelector('.h-header__dropdown');
+            const svgElement = button.querySelector('a svg');
+            
+
+            if(isDropdown) {
+            dropdown.toggleAttribute('show');
+            svgElement.toggleAttribute('animate');
+
+            }
+
+        })
+    });
+
+    window.addEventListener('DOMContentLoaded', function() {
+        headerButtons.forEach(( button ) => {
+            const isDropdown = button.querySelector('.h-header__dropdown') !== null;
+            const svgElement = button.querySelector('a svg');
+
+            if (isDropdown) {
+                svgElement.toggleAttribute('icon'); 
+            }
+        })
+    });
+
+    const menuHamburguer = $j('.h-header__nav > button');
+    const nav = $j('.h-header__links');
+
+    menuHamburguer.addEventListener('click', function() {
+        nav.toggleAttribute('active');
+        menuHamburguer.toggleAttribute('animate');
     })
-
-    button.addEventListener('mouseleave', function() {
-        const dropdown =  this.querySelector('.h-header__dropdown');
-        const svgElement = button.querySelector('a svg');
-
-        if(dropdown !== null && dropdown.hasAttribute('show')) {
-            dropdown.removeAttribute('show');
-            svgElement.removeAttribute('animate');
-        }
-    })
-});
-
-window.addEventListener('DOMContentLoaded', function() {
-    headerButtons.forEach(( button ) => {
-        const isDropdown = button.querySelector('.h-header__dropdown') !== null;
-        const svgElement = button.querySelector('a svg');
-
-        if (isDropdown) {
-            svgElement.toggleAttribute('icon'); 
-        }
-    })
-});
+}
